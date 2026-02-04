bf9e0e6d1bc4e8ad9545d32b953c41bd5d20ea37
Tanoue <M2520020@ngo.ohara.ac.jp>
2026-02-04 15:11:22 +0900

    form作った

--- js/main.js
+++ js/main.js
@@ -1,32 +1,19 @@
 //スライドメニュー
-const openBtn = document.querySelector("#menu-open"); //開くボタン
-const closeBtn = document.querySelector("#menu-close"); //閉じるボタン
-const menuPanel = document.querySelector("#menu-all"); //パネル全体
-const menuLists = document.querySelectorAll(".menu-list li"); //リスト配列
-// メニュー全体で使いまわすtimingOptions
-const menuOptions = {
-    duration: 1100, // アニメーション時間 1.4秒
-    easing:'ease', // アニメーション加速度
-    fill:'forwards', // 終了状態保持
-};
-
-// メニューを開く処理
-openBtn.addEventListener('click', () => {
-    // メニューパネルの表示処理
-    menuPanel.animate(
-        {
-            translate: ["100vw", 0],
-        },
-        menuOptions,
-    );
-});
-
-// メニューを閉じる処理
-closeBtn.addEventListener("click", () => {
-    menuPanel.animate(
-        {
-            translate: [0, "100vw"],
-        },
-        menuOptions,
-    );
-});
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+} )(jQuery);
